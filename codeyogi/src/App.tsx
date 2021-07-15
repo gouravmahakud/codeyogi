8aae7958dec6a613552c622d9011a27dae7af99f
Gourav Mahakud <gourav.srinu@gmail.com>
2021-07-15 16:30:43 +0530

    created react app

---
+++ codeyogi/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="text-6xl">
+      hi there
+    </div>
+  );
+}
+
+export default App;
